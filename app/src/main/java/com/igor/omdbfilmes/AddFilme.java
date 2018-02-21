8d3eaf0e5ce3a194959ee3a1367a26b183b6f729
Igor Cavalcante <igorcavlim@gmail.com>
2018-02-21 00:26:46 -0300

    adjusting layouts

--- app/src/main/java/com/igor/omdbfilmes/AddFilme.java
+++ app/src/main/java/com/igor/omdbfilmes/AddFilme.java
@@ -8,6 +8,7 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
