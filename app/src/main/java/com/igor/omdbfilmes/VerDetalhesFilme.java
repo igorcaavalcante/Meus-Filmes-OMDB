bb49d4f5dd2c346d927273ea7c061308256ea32f
Igor Cavalcante <igorcavlim@gmail.com>
2018-02-21 01:04:34 -0300

    add poster of movie

--- app/src/main/java/com/igor/omdbfilmes/VerDetalhesFilme.java
+++ app/src/main/java/com/igor/omdbfilmes/VerDetalhesFilme.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,6 +15,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class VerDetalhesFilme extends AppCompatActivity {
 
@@ -22,6 +24,7 @@ public class VerDetalhesFilme extends AppCompatActivity {
     private Button botaoExcluirFilme;
     private Button botaoVoltar;
     private String texto;
+    private ImageView imageViewCapaFilme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,21 +34,28 @@ public class VerDetalhesFilme extends AppCompatActivity {
         textViewDetalhes = (TextView) findViewById(R.id.textViewDetalhes);
         botaoExcluirFilme = (Button) findViewById(R.id.botaoExcluirFilme);
         botaoVoltar = (Button) findViewById(R.id.botaoVoltar);
+        imageViewCapaFilme = (ImageView) findViewById(R.id.imageViewCapaFilme);
 
         Intent activityAnterior = getIntent();
         final String idFilme = activityAnterior.getExtras().getString("id"); //pegando id do filme clicado na home
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference myRef = database.getReference("filmes").child(idFilme);
+        FirebaseDatabase databaseMostrarFilme = FirebaseDatabase.getInstance();
+        final DatabaseReference referenceFirebaseMostrarFilme = databaseMostrarFilme.getReference("filmes").child(idFilme);
 
-        myRef.addValueEventListener(new ValueEventListener() {
+        referenceFirebaseMostrarFilme.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                Filme f = dataSnapshot.getValue(Filme.class);
-                texto = "ID: " + f.getId() + " \nTítulo: " + f.getTitulo() + " \nAno: " + f.getAno() +
-                        " \nDiretor: " + f.getDiretor() + " \nDuração: " + f.getDuracao() + " \nAtores: "  + f.getAtores() +
-                        " \nClassificação OMDB: " + f.getClassificacao() + " \nGênero: " + f.getGenero() + " \nProdução: " + f.getProdutora();
-                textViewDetalhes.setText(texto);
+                try {
+                    Filme f = dataSnapshot.getValue(Filme.class);
+                    texto = "ID: " + f.getId() + " \nTítulo: " + f.getTitulo() + " \nAno: " + f.getAno() +
+                            " \nDiretor: " + f.getDiretor() + " \nDuração: " + f.getDuracao() + " \nAtores: " + f.getAtores() +
+                            " \nClassificação OMDB: " + f.getClassificacao() + " \nGênero: " + f.getGenero() + " \nProdução: " + f.getProdutora();
+                    textViewDetalhes.setText(texto);
+
+                    Picasso.with(VerDetalhesFilme.this).load(f.getCapa()).into(imageViewCapaFilme);
+                }catch(Exception e){
+                    System.out.print(e.getMessage());
+                }
             }
 
             @Override
@@ -62,7 +72,9 @@ public class VerDetalhesFilme extends AppCompatActivity {
         botaoExcluirFilme.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                myRef.child(idFilme).removeValue(); //excluindo filme do firebase
+                FirebaseDatabase databaseExcluirFilme = FirebaseDatabase.getInstance();
+                DatabaseReference referenceFirebaseExcluirFilme = databaseExcluirFilme.getReference("filmes");
+                referenceFirebaseExcluirFilme.child(idFilme).removeValue(); //excluindo filme do firebase
                 Toast.makeText(VerDetalhesFilme.this, "Filme removido com sucesso.", Toast.LENGTH_SHORT).show();
                 voltarTela();
             }
